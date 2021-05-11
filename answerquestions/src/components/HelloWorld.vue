d9d61bdcd3b553c0d849ce944adf6ffebfe041b9
FixedSpecialty <andremurphy98@outlook.com>
2021-05-10 18:21:07 -0600

    first commit

---
+++ answerquestions/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Default form login -->
+  <form>
+    <p class="h4 text-center mb-4">Need help answering questions? Ask the caterpillar!</p>
+    <label for="question" class="grey-text"></label>
+    <input type="text" id="questionbox" class="form-control"/>
+    <br/>
+    
+    <div class="text-center mt-4">
+      <button class="btn btn-indigo" type="submit">Submit</button>
+    </div>
+  </form>
+  <!-- Default form login -->
+</template>
